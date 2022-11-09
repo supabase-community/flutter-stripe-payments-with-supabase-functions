66affafcb77d5179b0f142d05723b64bcd9a87b0
Tyler <18113850+dshukertjr@users.noreply.github.com>
2022-11-09 16:50:30 +0900

    chore: update functions to use supabase-js v2

--- supabase/functions/_utils/supabase.ts
+++ supabase/functions/_utils/supabase.ts
@@ -1,33 +1,28 @@
-import { Customer } from "./types.ts";
+import { Database } from "./db_types.ts";
 import { stripe } from "./stripe.ts";
-import { jwtDecoder } from "./utils.ts";
 // Import Supabase client
-import { createClient } from "https://esm.sh/@supabase/supabase-js@^1.33.1";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.0.0";
 
-const supabaseAdmin = createClient(
-  // Supabase API URL - env var exported by default.
+// WARNING: The service role key has admin priviliges and should only be used in secure server environments!
+const supabaseAdmin = createClient<Database>(
   Deno.env.get("SUPABASE_URL") ?? "",
-  // Supabase API SERVICE ROLE KEY - env var exported by default.
-  // WARNING: The service role key has admin priviliges and should only be used in secure server environments!
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""
 );
 
 export const createOrRetrieveCustomer = async (authHeader: string) => {
-  // Create a Supabase client with the Auth context of the logged in user.
-  const supabaseClient = createClient(
-    // Supabase API URL - env var exported by default.
-    Deno.env.get("SUPABASE_URL") ?? "",
-    // Supabase API ANON KEY - env var exported by default.
-    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
-    // Create client with Auth context of the user that called the function.
-    // This way your row-level-security (RLS) policies are applied.
-    { headers: { Authorization: authHeader } }
-  );
+  // Get JWT from auth header
+  const jwt = authHeader.replace("Bearer ", "");
+  // Get the user object
+  const {
+    data: { user },
+  } = await supabaseAdmin.auth.getUser(jwt);
+  if (!user) throw new Error("No user found for JWT!");
 
   // Check if the user already has a Stripe customer ID in the Database.
-  const { data, error } = await supabaseClient
-    .from<Customer>("customers")
-    .select("*");
+  const { data, error } = await supabaseAdmin
+    .from("customers")
+    .select("stripe_customer_id")
+    .eq("id", user?.id);
   console.log(data?.length, data, error);
   if (error) throw error;
   if (data?.length === 1) {
@@ -37,20 +32,16 @@ export const createOrRetrieveCustomer = async (authHeader: string) => {
     return customer;
   }
   if (data?.length === 0) {
-    // No customer yet, let's create it.
-    const jwt = authHeader.split("Bearer ")[1];
-    const jwtUser = jwtDecoder(jwt);
-    const { sub: uid, email } = jwtUser;
     // Create customer object in Stripe.
     const customer = await stripe.customers.create({
-      email,
-      metadata: { uid },
+      email: user.email,
+      metadata: { uid: user.id },
     });
-    console.log(`New customer "${customer.id}" created for user "${uid}"`);
+    console.log(`New customer "${customer.id}" created for user "${user.id}"`);
     // Insert new customer into DB
     await supabaseAdmin
-      .from<Customer>("customers")
-      .insert({ id: uid, stripe_customer_id: customer.id })
+      .from("customers")
+      .insert({ id: user.id, stripe_customer_id: customer.id })
       .throwOnError();
     return customer.id;
   } else throw new Error(`Unexpected count of customer rows: ${data?.length}`);
